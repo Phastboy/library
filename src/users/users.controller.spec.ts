5d715adfa7d770c4ad16538d6241d228c24ba4de
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2024-12-25 21:24:08 +0100

    test(users): add unit tests for UsersController
    - Added unit tests for `UsersController` to ensure proper functionality.
    - Mocked `UsersService` methods for testing.
    - Corrected import paths in `UsersController` for consistency.

---
+++ src/users/users.controller.spec.ts
@@ -0,0 +1,40 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { RoleGuard } from '../auth/role.guard';
+import { AuthGuard } from '../auth/auth.guard';
+
+jest.mock('../utils/response.util');
+
+describe('UsersController', () => {
+  let controller: UsersController;
+  let usersService: UsersService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [UsersController],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: {
+            findAll: jest.fn(),
+            find: jest.fn(),
+            delete: jest.fn(),
+          },
+        },
+      ],
+    })
+      .overrideGuard(AuthGuard)
+      .useValue({ canActivate: jest.fn(() => true) })
+      .overrideGuard(RoleGuard)
+      .useValue({ canActivate: jest.fn(() => true) })
+      .compile();
+
+    controller = module.get<UsersController>(UsersController);
+    usersService = module.get<UsersService>(UsersService);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
