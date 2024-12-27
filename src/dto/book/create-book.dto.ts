5758053f8da77e23e05ac27cd6b0ec2a674ff138
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2024-12-27 20:04:56 +0100

    Add swagger docs to create-book.dto.ts

--- src/dto/book/create-book.dto.ts
+++ src/dto/book/create-book.dto.ts
@@ -1,29 +1,57 @@
 import { IsString, IsInt, IsOptional, Min, MaxLength } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class CreateBookDto {
+  @ApiProperty({
+    description: 'The title of the book',
+    maxLength: 255,
+    example: 'The Great Gatsby',
+  })
   @IsString()
   @MaxLength(255)
   title: string;
 
+  @ApiPropertyOptional({
+    description: 'A brief description of the book',
+    example: 'A novel about the American dream and societal excess.',
+  })
   @IsString()
   @IsOptional()
-  description: string;
+  description?: string;
 
+  @ApiProperty({
+    description: 'The author of the book',
+    maxLength: 255,
+    example: 'F. Scott Fitzgerald',
+  })
   @IsString()
   @MaxLength(255)
   author: string;
 
+  @ApiProperty({
+    description: 'The genre of the book',
+    maxLength: 100,
+    example: 'Fiction',
+  })
   @IsString()
   @MaxLength(100)
   genre: string;
 
-  @IsOptional()
+  @ApiProperty({
+    description: 'The ISBN of the book',
+    maxLength: 20,
+    example: '978-0743273565',
+  })
   @IsString()
   @MaxLength(20)
-  ISBN?: string;
+  ISBN: string;
 
-  @IsOptional()
+  @ApiProperty({
+    description: 'The total number of copies available',
+    minimum: 1,
+    example: 5,
+  })
   @IsInt()
   @Min(1)
-  totalCopies?: number;
+  totalCopies: number;
 }
