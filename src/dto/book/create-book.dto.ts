cd30c7060e24d63998d0c3ad692ae3216cb8e285
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2024-12-27 12:37:25 +0000

    feat(book): create book dto

--- src/dto/book/create-book.dto.ts
+++ src/dto/book/create-book.dto.ts
@@ -1,1 +1,29 @@
-export class CreateBookDto {}
+import { IsString, IsInt, IsOptional, Min, MaxLength } from 'class-validator';
+
+export class CreateBookDto {
+  @IsString()
+  @MaxLength(255)
+  title: string;
+
+  @IsString()
+  @IsOptional()
+  description: string;
+
+  @IsString()
+  @MaxLength(255)
+  author: string;
+
+  @IsString()
+  @MaxLength(100)
+  genre: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(20)
+  ISBN?: string;
+
+  @IsOptional()
+  @IsInt()
+  @Min(1)
+  totalCopies?: number;
+}
